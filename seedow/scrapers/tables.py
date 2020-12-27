aa225b45a043d99fdcb5d78927d396b08392be5d
Ryan Whittingham <ryanwhittingham89@gmail.com>
2020-12-27 13:14:27 -0800

    Monkey-patched wikitablescrape.parse.HtmlTable.parse_headers

--- seedow/scrapers/tables.py
+++ seedow/scrapers/tables.py
@@ -1,6 +1,30 @@
 from wikitablescrape.parse import *
 
 
+def _parse_header(self):
+    """Monkey-patched version of
+    :code:`wikitablescrape.parse.HtmlTable.parse_headers`. Doesn't
+    combine headers and sub-headers into one string; returns a
+    dictionary instead.
+    """
+    headers = {}
+    caption = self.tag.find('caption')
+    if caption:
+        headers['header'] = clean_cell(caption)
+        return headers
+
+    h2 = self.tag.findPrevious('h2')
+    if h2:
+        headers['header'] = clean_cell(h2)
+        # Try to find a subheader as well
+        h3 = self.tag.findPrevious('h3')
+        if h3:
+            headers['subheader'] = clean_cell(h3)
+        return headers
+
+    return None
+
+
 class Parser(Parser):
     """Simple wrapper around :code:`wikitablescrape.parse.Parser` that
     adds some more helpful methods.
@@ -30,3 +54,7 @@ class Parser(Parser):
         or None.
         """
         return [x['header'] for x in self.parsed_tables]
+
+
+# Apply monkey patch
+HtmlTable.parse_header = _parse_header
